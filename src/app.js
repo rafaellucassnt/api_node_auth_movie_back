90bc994be1f58d568f7d002768377b43cb8df580
Rafael Lucas <rafael.lucas.4400@gmail.com>
2023-06-27 01:27:49 -0300

    [feat] Implementação das rotas de filmes e usuarios

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const moviesRouter = require('./routes/movies');
+const usersRouter = require('./routes/users');
+
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Exercício 3 - API completa com Front End básico <br>Brenna Lopes Farace <br>Danilo Augusto Pereira <br>Rafael da Mota Corrêa <br>Rafael da Silveira <br>Rafael Lucas dos Santos')
 })
+
+app.use('/filmes', moviesRouter);
+app.use('/usuarios', usersRouter);
 
 app.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`)
