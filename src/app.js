005c9537bbdb5c14cf68930030a87538a27d146a
Rafael Lucas <rafael.lucas.4400@gmail.com>
2023-06-30 00:30:03 -0300

    [fix] import cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const moviesRouter = require('./routes/movies');
 const usersRouter = require('./routes/users');
